302fdeacb50fd2e95e4e235feed78c80ebf3921e
Kelly Paxton <klypxtn@gmail.com>
2025-01-03 02:12:45 -0700

    Now you can guess as many tries as you'd like

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import random
 from modules.ls import ls
 
 # computer chooses a random number
-x = random.choice(ls)  
-
-y = int(input('Guess a number between 1-10: '))
+x = random.choice(ls) 
 
 # checks if the number you chose is the same number the computer chose
-if x == y:                      
-    print('Good Job! The number was ')
-    print(x)
-else:
-    print('Sorry, the number was ')
-    print(x)
+while True:
+    y = int(input('Guess a number between 1-10: '))
+    if y != x:
+        print("Sorry, Try Again!")
+    else:
+        print("Great Job! The number Was ", x)
+        break
